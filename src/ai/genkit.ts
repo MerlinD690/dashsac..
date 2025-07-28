4653983b43eed66c4dd17dde78b0fea535883b48
Lucas Evangelista <lucasevangelista690@gmail.com>
2025-07-28 00:15:53 +0000

    eu mexi na pasta genkit.ts porque a iA não tava funcionando
    deixei des

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,8 @@
-import { Genkit } from '@genkit-ai/ai';
-import { googleAi } from '@genkit-ai/googleai';
+import { genkit } from 'genkit';
+import { googleAI } from '@genkit-ai/googleai';
 
-export const ai = new Genkit({
-  apis: [googleAi({ key: process.env.GEMINI_API_KEY })],
-  defaultApis: [googleAi()],
-  defaultModel: 'gemini-pro',
+export const ai = genkit({
+  plugins: [googleAI({ apiKey: process.env.GEMINI_API_KEY })],
+  logSinks: [],
+  enableTracing: true,
 });
